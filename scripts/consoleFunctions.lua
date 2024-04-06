563e0dba1c1b104fc74b7273ce062210237fc68a
Zerpnord <pyxledev0@gmail.com>
2024-04-06 13:27:55 +0300

    run_script function in devConsole

--- scripts/consoleFunctions.lua
+++ scripts/consoleFunctions.lua
@@ -1,6 +1,6 @@
 local consoleFunctions = {
     funcsList = {
-        "assign",
+        "assign", "run_script",
     }
 }
 
@@ -31,6 +31,32 @@ function consoleFunctions.assignScript(devConsole, command, i)
     local assignCommand = temp
     devConsole.assignedCommands[#devConsole.assignedCommands+1] = assignCommand
     devConsole.assignedKeys[#devConsole.assignedKeys+1] = assignedKey
+end
+
+function consoleFunctions.run_scriptScript(devConsole, command, i)
+    i = i + 1
+    --Skip spaces
+    while string.sub(command, i, i) == " " do
+        i = i + 1
+    end
+    --Read script path
+    local temp = ""
+    while string.sub(command, i, i) ~= " " do
+        --Check for incorrect writing
+        if i > #command then
+            break
+        end
+        temp = temp .. string.sub(command, i, i)
+        i = i + 1
+    end
+    --Read file from given path
+    local scriptFile = love.filesystem.read(temp)
+    if scriptFile then
+        local commands = devConsole:readCommandsFromInput(scriptFile)
+        for k = 1, #commands do
+            RunConsoleCommand(commands[k])
+        end
+    end
 end
 
 return consoleFunctions
